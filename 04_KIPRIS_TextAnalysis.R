8d9dc228b43ba050d570a46f3a754447c64f2c24
Keungoui Kim <56111110+awekim@users.noreply.github.com>
2024-11-05 23:27:06 +0900

    Add files via upload

--- 04_KIPRIS_TextAnalysis.R
+++ 04_KIPRIS_TextAnalysis.R
@@ -12,15 +12,24 @@
 ##  Notice :
 #######################################################################
 
+library(dplyr)
+
 ### Load Data
 load(file="R file/cpc.trend.all.RData")
+
+country.code <- 'KR'
 
 ### Abstract
 load(file=paste0("R file/",country.code,"/Abstract.RData"))
 
+### Priority
+load(file=paste0("R file/",country.code,"/Priority.RData"))
+Priority$Year <- substr(Priority$우선권주장출원일자,1,4)
+
 # remove NA columns
 Abstract <- Abstract[,is.na(names(Abstract))==FALSE]
 nrow(Abstract) # 4,903,079
+Abstract %>% head(1)
 
 # Remove missing abstracts
 Abstract<- Abstract[!grepl('내용 없음',Abstract$초록),]
@@ -44,8 +53,19 @@ Abstract$초록 <-
   gsub("\\s+", " ", Abstract$초록)
 
 Abstract.trend <- cpc.trend.all %>% select(출원번호,type) %>% unique %>% 
-  left_join(Abstract)
+  left_join(Abstract) %>%
+  left_join(Priority %>% select(출원번호,Year) %>% unique)
+rm(Priority)
+
+Abstract.trend$Year %>% table
+length(unique(Abstract.trend$출원번호)) # 13,064
+
 rm(Abstract)
 save(Abstract.trend, file="R file/Abstract.trend.RData")
-rm(Abstract.trend)
+load(file="R file/Abstract.trend.RData")
+write.csv(Abstract.trend, file="Abstract_trend.csv", row.names=FALSE)
 
+cpc.trend.all$출원번호 %>% unique %>% length # 52672
+Abstract.trend$출원번호 %>% unique %>% length # 52672
+
+rm(cpc.trend.all, Abstract.trend)
